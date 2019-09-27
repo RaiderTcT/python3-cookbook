96c67e167ac1ddfb044acb7632477ffa91f7ff9f
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-09-27 16:50:49 +0800

    Update p12_mostfreq_items.py
    Counter的使用

--- cookbook/c01/p12_mostfreq_items.py
+++ cookbook/c01/p12_mostfreq_items.py
@@ -1,10 +1,51 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+# -*-coding:utf-8-*-
 """
-Topic: 查找出现次数最多的元素
-Desc : 
+序列中出现次数最多的元素， Counter
+一个 Counter 是一个 dict 的子类，用于计数可哈希对象。
+它是一个集合，元素像字典键(key)一样存储，它们的计数存储为值。
+计数可以是任何整数值，包括0和负数。
 """
+from collections import Counter
 
+
+words = """A Counter is a dict subclass for counting hashable objects.
+    It is a collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
+    Counts are allowed to be any integer value including zero or negative counts. The Counter class is similar to bags
+    or multisets in other languages.
+    """
+gen_1  = (item for item in range(1, 10))
+
+
+cnt = Counter(gen_1)
+for k, v in cnt.items():
+    print(k, v)
+print(cnt)  # 每个字符出现的次数
+print(cnt.most_common(3))  # 出现频率最高的3个字符 [(' ', 69), ('e', 30), ('s', 25)]
+print(cnt.most_common()[:-3:-1]) # 最低的2个字符
+print(cnt['c'])  # c的出现次数
+print(sum(cnt.values()))  # total
+
+
+c = Counter(a=4, b=2, c=0, d=-2)
+print(sorted(c.elements()))
+more_words = ['c', 'd', 'c']
+c.update(more_words)
+print(c)
+print(+c)  # remove zero and negative counts
+# 单目加和减（一元操作符）意思是从空计数器加或者减去。
+print(-c)
+
+print("="*30)
+cnt_1 = Counter(a=3, b=2, c=1)
+cnt_2 = Counter(a=1, b=3, c=2)
+
+print(cnt_1 + cnt_2)  # cnt_1[x] + cnt_2[x]
+print(cnt_1 - cnt_2)  # 相减 只保留正数
+print(cnt_1 & cnt_2)  # intersection:  min(cnt_1[x], cnt_2[x]) # doctest: +SKIP
+print(cnt_1 | cnt_2)  # union max(cnt_1[x], cnt_2[x])
+
+print("="*30)
 
 def most_freqency():
     words = [
@@ -22,4 +63,3 @@ def most_freqency():
 
 if __name__ == '__main__':
     most_freqency()
-
