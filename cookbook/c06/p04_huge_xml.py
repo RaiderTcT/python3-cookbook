9397d3f726f58de65c38c8fb882fe6659b761a39
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-10-09 10:59:05 +0800

    Update p04_huge_xml.py
    增量解析xml iterpaser

--- cookbook/c06/p04_huge_xml.py
+++ cookbook/c06/p04_huge_xml.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+# -*- coding: utf-8  -*-
 """
-Topic: 增量式的处理大型XML文档
-Desc : 
+使用增量方式解析大型xml文件
 """
 from xml.etree.ElementTree import iterparse
-from xml.etree.ElementTree import parse
-from collections import Counter
-
+from xml_namespace import XMLNamespaces
 
 def parse_and_remove(filename, path):
     path_parts = path.split('/')
-    doc = iterparse(filename, ('start', 'end'))
-    # Skip the root element
-    next(doc)
+    # 增量式处理， 提供文件名和事件列表（start/end）
+    parse = iterparse(filename, ("start", "end"))
 
+    next(parse)  # 跳过了root元素
     tag_stack = []
     elem_stack = []
-    for event, elem in doc:
+    for event, elem in parse:  # 生成(event, elem)的元组
         if event == 'start':
             tag_stack.append(elem.tag)
             elem_stack.append(elem)
         elif event == 'end':
             if tag_stack == path_parts:
                 yield elem
-                elem_stack[-2].remove(elem)
+                elem_stack[-2].remove(elem)  # 从父节点中移除当前元素
             try:
-                tag_stack.pop()
-                elem_stack.pop()
+                elem_stack.pop()  # 相应地从栈中移除yield返回的元素
+                tag_stack.pop()  # 等待下一个需要的element
             except IndexError:
                 pass
+
+
+ns_1 = XMLNamespaces(default="urn:schemas-microsoft-com:office:spreadsheet",
+                    o="urn:schemas-microsoft-com:office:office",
+                    x="urn:schemas-microsoft-com:office:excel",
+                    dt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882",
+                    ss="urn:schemas-microsoft-com:office:spreadsheet")
+
+data = parse_and_remove("test.xml",
+                         ns_1('{x}ExcelWorkbook/{x}WindowHeight'))
+data_1 = parse_and_remove("test.xml",
+        ns_1("{ss}Worksheet/{ss}Table/{ss}Row/{ss}Cell/{ss}Data"))
+
+for item in data_1:
+    print(item.text)
+
+
 
 
 def huge_xml():
