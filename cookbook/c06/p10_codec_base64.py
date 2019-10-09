89202c1b2706f06b99b86146e056a8aa0982119b
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-10-09 11:41:38 +0800

    Update p10_codec_base64.py

--- cookbook/c06/p10_codec_base64.py
+++ cookbook/c06/p10_codec_base64.py
@@ -8,17 +8,17 @@ import base64
 
 
 def codec_base64():
-    s = b'hello'
+    # 编码和解码的结果都是bytes 或bytearray类型
+    s = b"Hello World"
+    a = base64.b64encode(s)  # 只能处理字节串和字节数组
+    print(a)  # b'SGVsbG8gV29ybGQ='
 
-    # Encode as Base64
-    a = base64.b64encode(s)
-    print(a)
+    s_1 = bytearray(b'hello')
+    print(s_1)  # bytearray(b'hello')
+    a_1 = base64.b64encode(s_1)
+    print(a_1, a_1.decode('ascii'))  # b'aGVsbG8=' aGVsbG8=
 
-    # Decode from Base64
-    b = base64.b64decode(a)
-    print(b)
+    print(base64.b64decode(a).decode('utf-8'))  # Hello World
 
 if __name__ == '__main__':
     codec_base64()
-
-
