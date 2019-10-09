40a7ab59ab0a2b654af9057ab17c0b2a92024669
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-10-09 16:43:23 +0800

    Update p05_default_argument.py
    函数默认参数

--- cookbook/c07/p05_default_argument.py
+++ cookbook/c07/p05_default_argument.py
@@ -11,9 +11,22 @@ def spam(a, b=42):
 spam(1) # Ok. a=1, b=42
 spam(1, 2) # Ok. a=1, b=2
 
+# 默认值是可变容器（列表、集合或字典），使用None作为默认值
+def spam_1(a, b=None):
+    if b is None:
+        b = []
+
 _no_value = object()
 
 def spam(a, b=_no_value):
     if b is _no_value:
         print('No b value supplied')
 
+x = 23
+
+def spam_3(a, b=x):  # 对默认参数的赋值只会在函数定义时绑定一次
+    print(a, b)
+
+spam_3(1)  # 1 23
+x = 44
+spam_3(34)  # 34 23
