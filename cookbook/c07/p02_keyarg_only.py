8f9640077e949e8cec9b547e6087b665c39ef219
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-10-09 16:10:06 +0800

    Update p02_keyarg_only.py
    *agrs

--- cookbook/c07/p02_keyarg_only.py
+++ cookbook/c07/p02_keyarg_only.py
@@ -4,7 +4,14 @@
 Topic: 只允许关键字形式的参数
 Desc : 
 """
+# *开头的参数只能是作为 最后一个位置参数, **开头的参数只能作为最后一个参数出现
+# 在*agrs之后的参数称为keyword-only参数， 既只能作为关键字参数出现
+def a(x, *args, y):  # *args 解开后的元组
+    print(args)  # 元组
 
+a(1, 2, 3, 4, 5, y=5)   # args = (2, 3, 4, 5)
+a(1, *(2, 3, 4, 5), y=3)  # args = (2, 3, 4, 5)
+a(1, (1, 2, 3), y =3)  # args= ((1, 2, 3), )
 
 def recv(maxsize, *, block):
     'Receives a message'
