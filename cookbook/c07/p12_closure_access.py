60b7bf2e3d7ce7ee3c2a84f766cb31db960feafd
Ulysses <38154485+RaiderTcT@users.noreply.github.com>
2019-10-15 17:30:48 +0800

    Update p12_closure_access.py

--- cookbook/c07/p12_closure_access.py
+++ cookbook/c07/p12_closure_access.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-"""
-Topic: 闭包访问函数内部变量
-Desc : 
-"""
-
+# @作者     ： Ulysses
+# @功能描述 ： 访问定义在闭包内的变量
+# @修改记录 ： 2019/2/15 11:48 创建
+import sys
 
 def sample():
     n = 0
-    # Closure function
-    def func():
-        print('n=', n)
 
-    # Accessor methods for n
+    def func():
+        print('n =', n)
+
     def get_n():
         return n
 
@@ -20,32 +16,27 @@ def sample():
         nonlocal n
         n = value
 
-    # Attach as function attributes
     func.get_n = get_n
     func.set_n = set_n
     return func
 
-f = sample()
-f()
-f.set_n(10)
-f()
 
-import sys
 class ClosureInstance:
     def __init__(self, locals=None):
         if locals is None:
-            locals = sys._getframe(1).f_locals
+            locals =sys._getframe(1).f_locals  # 获取调用当前函数的函数的局部变量
 
-        # Update instance dictionary with callables
-        self.__dict__.update((key,value) for key, value in locals.items()
-                            if callable(value) )
-    # Redirect special methods
+        # ClosureInstance实例的方法 会被替换成 locals的可调用方法
+        self.__dict__.update((key, value) for key, value in locals.items()
+                             if callable(value))
+
     def __len__(self):
         return self.__dict__['__len__']()
 
-# Example use
+
 def Stack():
     items = []
+
     def push(item):
         items.append(item)
 
@@ -54,13 +45,21 @@ def Stack():
 
     def __len__():
         return len(items)
-
+    print(sys._getframe().f_locals)  # 当前函数的局部便量
+    # 将内层数据拷贝到一个实例字典中返回
     return ClosureInstance()
 
-s = Stack()
-print(s)
-s.push(10)
-s.push(20)
-print(len(s))
-print(s.pop())
-print(s.pop())
+if __name__ == "__main__":
+
+    f = sample()
+    f()
+    f.set_n(11)
+    f()
+
+    s = Stack()
+    s.push(10)
+    s.push(20)
+    s.push('test')
+    print(len(s))
+    print(s.pop())
+    print(s.pop())
